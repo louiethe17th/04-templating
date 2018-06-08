e97ce744763bdaa9950a21ad0b32fe4ab3358e70
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-06-08 11:00:22 -0700

    completed article.js and index se.js and index syntax

--- rosie-tyler/scripts/article.js
+++ rosie-tyler/scripts/article.js
@@ -2,7 +2,7 @@
 
 let articles = [];
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -11,34 +11,34 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-let articleTemplate = $('#article-template').html();
-let templateShow = Handlebars.compile(articleTemplate);
+Article.prototype.toHtml = function () {
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let articleTemplate = $('#article-template').html();
+  let templateShow = Handlebars.compile(articleTemplate);
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
-  return templateShow(this);
+
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
 
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
-
-  // REVIEW: The ternary operator above accomplishes this same logic.
+  // DONE-REVIEW: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
   //   this.publishStatus = `published ${this.daysAgo} days ago`;
   // } else {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return templateShow(this);
 
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
-rawData.sort((a,b) => {
+// You have to wrap the inputs in an arrow function only when there is more than one input. 
+rawData.sort((a, b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
